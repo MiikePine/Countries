69336e85b5731ad930b30ff5a6c7cd92a2dc298b
miikepine <miikepiine@gmail.com>
2024-02-04 11:45:32 +0100

    api working

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,4 +1,5 @@
 import Header from "./Header";
+import { useEffect, useState } from "react";
 import { useParams } from 'react-router-dom';
 import data from '../database/data.json';
 import { NavLink } from 'react-router-dom'; 
@@ -11,8 +12,23 @@ import PropTypes from 'prop-types';
 function Countries({darkMode, toggleDarkMode}) {
     const { countryName } = useParams();
 
-    const selectedCountry = data.find((country) => country.name === countryName);
+    const [selectedCountry, setSelectedCountry] = useState(null);
 
+
+    useEffect(() => {
+      const fetchCountryData = async () => {
+        try {
+          const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}`);
+          const data = await response.json();
+          console.log(" countries api",data); // Log dos dados retornados pela API para o país selecionado
+          setSelectedCountry(data[0]); // Defina o país selecionado com os dados retornados
+        } catch (error) {
+          console.error('Error fetching country data:', error);
+        }
+      };
+    
+      fetchCountryData();
+    }, [countryName]);
 
   return (
     <>
@@ -27,13 +43,14 @@ function Countries({darkMode, toggleDarkMode}) {
 </button>
 </NavLink>   
                 </div>
+                {selectedCountry && (
 <div className="md:flex  w-full mt-24">
       <div className=" flex items-center justify-center align-middle mx-10 md:mx-0  md:w-1/2">
-        <img src={selectedCountry.flag} alt={selectedCountry.name}     className="w-full  h-auto mb-2 max-w-sm md:max-w-lg"/>
+      <img src={selectedCountry.flags && selectedCountry.flags.png} alt={selectedCountry.name && selectedCountry.name.common} className="w-full h-auto mb-2 max-w-sm md:max-w-lg" />
       </div>
       <div>
         <div>
-            <p className="font-bold text-2xl py-1 pt-10 mx-10 md:mx-0  mb-6">{selectedCountry.name}</p>
+            <p className="font-bold text-2xl py-1 pt-10 mx-10 md:mx-0  mb-6">{selectedCountry.name.common}</p>
         </div>
 
         <div className="mx-10 md:mx-0 md:flex justify-between">
@@ -43,22 +60,40 @@ function Countries({darkMode, toggleDarkMode}) {
                 <p><span className={`font-semibold text-xs py-1 ${darkMode ? 'text-white' : 'text-gray-700'}`}>Sub Region:</span><span className='text-xs text-gray-500'> {selectedCountry.subregion}</span></p>
                 <p><span className={`font-semibold text-xs py-1 ${darkMode ? 'text-white' : 'text-gray-700'}`}>Region:</span><span className='text-xs text-gray-500'> {selectedCountry.region}</span></p>
                 <p><span className={`font-semibold text-xs py-1 ${darkMode ? 'text-white' : 'text-gray-700'}`}>Capital:</span><span className='text-xs text-gray-500'> {selectedCountry.capital}</span></p>
+                <p><span className={`font-semibold text-xs py-1 ${darkMode ? 'text-white' : 'text-gray-700'}`}>Capital:</span><span className='text-xs text-gray-500'> {selectedCountry.capital}</span></p>
+
             </div>
 
             <div className=" md:mx-0 md:ml-20 w-1/2">
-            <p><span className={`font-semibold text-xs py-1 ${darkMode ? 'text-white' : 'text-gray-700'}`}>Top Level Domain:</span><span className='text-xs text-gray-500'> {selectedCountry.topLevelDomain}</span></p>
-            <p><span className={`font-semibold text-xs py-1 ${darkMode ? 'text-white' : 'text-gray-700'}`}>Currencies:</span><span className='text-xs text-gray-500'> {selectedCountry.currencies[0].code}</span></p>
-            <p>
+            <div>
+ 
+</div>            <p>
+  <span className={`font-semibold text-xs py-1 ${darkMode ? 'text-white' : 'text-gray-700'}`}>
+    Currencies:
+  </span>
+  <span className='text-xs text-gray-500'>
+    {Object.keys(selectedCountry.currencies).map(currencyCode => (
+      <span key={currencyCode}>
+        {selectedCountry.currencies[currencyCode].name} ({selectedCountry.currencies[currencyCode].symbol})
+      </span>
+    ))}
+  </span>
+</p>      
+
+{selectedCountry.languages && typeof selectedCountry.languages === 'object' && (
+  <p>
     <span className={`font-semibold text-xs py-1 ${darkMode ? 'text-white' : 'text-gray-700'}`}>Languages: </span>
     <span className='text-xs text-gray-500'>
-        {selectedCountry.languages && selectedCountry.languages.map((language, index) => (
-            <span key={index}>
-                {language.name}
-                {index < selectedCountry.languages.length - 1 && ', '}
-            </span>
-        ))}
+      {Object.keys(selectedCountry.languages).map((languageCode, index) => (
+        <span key={index}>
+          {selectedCountry.languages[languageCode]}
+          {index < Object.keys(selectedCountry.languages).length - 1 && ', '}
+        </span>
+      ))}
     </span>
-</p>
+  </p>
+)}
+
             </div >
       </div>
 
@@ -68,6 +103,7 @@ function Countries({darkMode, toggleDarkMode}) {
       </div>
       </div>
 </div> 
+                )}
       </div>
 
     </>
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,9 +1,7 @@
 import { useState, useEffect } from 'react';
-import { Listbox } from '@headlessui/react'
-import data from '../database/data.json';
+// import data from '../database/data.json';
 // import { MdOutlineSearch } from "react-icons/md";
-import { IoMoonOutline } from "react-icons/io5";
-import { MdKeyboardArrowDown } from "react-icons/md";
+
 import { NavLink } from 'react-router-dom'; 
 import Header from './Header';
 import Search from './Search';
@@ -16,24 +14,34 @@ function Dashboard({ darkMode }) {
   const [selectedContinent, setSelectedContinent] = useState({ name: 'Filter by Country' });
   const [searchTerm, setSearchTerm] = useState('');
 
-
-
   useEffect(() => {
-    setFilteredCountries(data);
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://restcountries.com/v3.1/all');
+        const data = await response.json();
+        console.log(data); // Log dos dados etornados pela API
+        setFilteredCountries(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+  
+    fetchData();
   }, []);
 
   useEffect(() => {
-    let filtered = data;
+    let filtered = [...filteredCountries]; // Cria uma nova cópia do array
     if (searchTerm) {
       filtered = filtered.filter(country =>
-        country.name.toLowerCase().includes(searchTerm.toLowerCase())
+        country.name && country.name.common.toLowerCase().includes(searchTerm.toLowerCase())
       );
     }
     if (selectedContinent.name !== 'Filter by Country') {
       filtered = filtered.filter(country => country.region === selectedContinent.name);
     }
     setFilteredCountries(filtered);
-  }, [searchTerm, selectedContinent]);
+  }, [searchTerm, selectedContinent]);// Remova filteredCountries da lista de dependências
+
 
   const handleContinentChange = (selectedContinent) => {
     setSelectedContinent(selectedContinent);
@@ -57,14 +65,15 @@ function Dashboard({ darkMode }) {
 
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 xxl:grid-cols-6 gap-16 mt-10 mx-10 md:mx-20">
         {filteredCountries.map((country, index) => (
-            <NavLink key={index} to={`/countries/${country.name}`}>
+            <NavLink key={index} to={`/countries/${country.name.common}`}>
             <div className={`rounded-lg shadow-md cursor-pointer h-full ${darkMode ? 'bg-[#2b3945]' : 'bg-white'}`}>
-              <img src={country.flag} alt={country.name} className="w-full h-40 object-cover mb-2" />
+              <img src={country.flags.png} alt={country.name.common} className="w-full h-40 object-cover mb-2" />
               <div className='mx-6 mt-6 mb-10 '>
-                <p className={`font-bold text-md py-1 ${darkMode ? 'text-white' : 'text-[#2b3945]'}`}>{country.name}</p>
+                <p className={`font-bold text-md py-1 ${darkMode ? 'text-white' : 'text-[#2b3945]'}`}>{country.name.common}</p>
                 <p><span className={`font-semibold text-xs py-1 text-gray-700 ${darkMode ? 'text-white' : 'text-[#2b3945]'}`}>Population:</span><span className='text-xs text-gray-500'> {country.population}</span></p>
                 <p><span className={`font-semibold text-xs py-1 text-gray-700 ${darkMode ? 'text-white' : 'text-[#2b3945]'}`}>Region:</span><span className='text-xs text-gray-500'> {country.region}</span></p>
                 <p><span className={`font-semibold text-xs py-1 text-gray-700 ${darkMode ? 'text-white' : 'text-[#2b3945]'}`}>Capital:</span><span className='text-xs text-gray-500'> {country.capital}</span></p>
+
               </div>
             </div>
           </NavLink>
